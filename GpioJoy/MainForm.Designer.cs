e085c773d7e6768f672a5ae71b401cca61fdddb8
Graham Briggs <GrahamBriggs@LittleBytesOfPi.com>
2022-12-04 09:18:14 -0800

    Fixing font on main page

--- GpioJoy/MainForm.Designer.cs
+++ GpioJoy/MainForm.Designer.cs
@@ -593,6 +593,7 @@
             this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(828, 489);
             this.Controls.Add(this.tabControlRobot);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.KeyPreview = true;
             this.MaximumSize = new System.Drawing.Size(844, 528);
             this.MinimumSize = new System.Drawing.Size(844, 528);
